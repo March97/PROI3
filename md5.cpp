8a0471958ce74c18d1601599e235916046e4e2a5
March97 <dominikm97@wp.pl>
2017-05-31 00:17:55 +0200

    0.2
    przejscia autmatu i funkcje md5

--- md5.cpp
+++ md5.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 #include "md5.h"
 
+// stale dla automatu md5
+#define S11 7
+#define S12 12
+#define S13 17
+#define S14 22
+#define S21 5
+#define S22 9
+#define S23 14
+#define S24 20
+#define S31 4
+#define S32 11
+#define S33 16
+#define S34 23
+#define S41 6
+#define S42 10
+#define S43 15
+#define S44 21
+
+// F, G, H, I funkcje md5
+inline md5::uint4 md5::F(uint4 x, uint4 y, uint4 z) {
+  return x&y | ~x&z;
+}
+
+inline md5::uint4 md5::G(uint4 x, uint4 y, uint4 z) {
+  return x&z | y&~z;
+}
+
+inline md5::uint4 md5::H(uint4 x, uint4 y, uint4 z) {
+  return x^y^z;
+}
+
+inline md5::uint4 md5::I(uint4 x, uint4 y, uint4 z) {
+  return y ^ (x | ~z);
+}
+
+// rotate_left rotates x left n bits.
+inline md5::uint4 md5::rotate_left(uint4 x, int n) {
+  return (x << n) | (x >> (32-n));
+}
+
+// FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
+// Rotation is separate from addition to prevent recomputation.
+inline void md5::FF(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
+  a = rotate_left(a+ F(b,c,d) + x + ac, s) + b;
+}
+
+inline void md5::GG(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
+  a = rotate_left(a + G(b,c,d) + x + ac, s) + b;
+}
+
+inline void md5::HH(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
+  a = rotate_left(a + H(b,c,d) + x + ac, s) + b;
+}
+
+inline void md5::II(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
+  a = rotate_left(a + I(b,c,d) + x + ac, s) + b;
+}
+
 md5::md5()
 {
     init();
--- md5.h
+++ md5.h
@@ -4,14 +4,28 @@ class md5
 {
 private:
 
+    typedef unsigned char uint1; //  8bit
+    typedef unsigned int uint4;  // 32bit
+
     enum {blocksize = 64}; //musi tyle byc enum-typ wyliczeniowy
     bool finalized;
-    unsigned char buffer[blocksize]; // bajty ktore nie zmiescily sie w 64 ostatnich bitach
-    unsigned int count[2];   // 64 bitowy licznik liczby bitow (low, hiigh)
-    unsigned int state[4];   // ABCD
-    unsigned char digest[16]; // wynik
+    uint1 buffer[blocksize]; // bajty ktore nie zmiescily sie w 64 ostatnich bitach
+    uint4 count[2];   // 64 bitowy licznik liczby bitow (low, hiigh)
+    uint4 state[4];   // ABCD
+    uint1 digest[16]; // wynik
 
     void init(); //funkcja inicjalizacycja wyk w konstruktorach
+
+    //operacje logiczne
+    static inline uint4 F(uint4 x, uint4 y, uint4 z);
+    static inline uint4 G(uint4 x, uint4 y, uint4 z);
+    static inline uint4 H(uint4 x, uint4 y, uint4 z);
+    static inline uint4 I(uint4 x, uint4 y, uint4 z);
+    static inline uint4 rotate_left(uint4 x, int n);
+    static inline void FF(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+    static inline void GG(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+    static inline void HH(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+    static inline void II(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
 
 public:
 
